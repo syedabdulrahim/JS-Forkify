cd3c640db992b6a1af307ee7a6b23da048509265
Syed abdul rahim <syedabdulrahim4@gmail.com>
2020-04-17 17:07:44 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path=require('path')
+
+const HtmlWebpackpPlugin=require('html-webpack-plugin');
+
+
+module.exports={
+
+    entry:['babel-polyfill','./src/js/index.js'],
+     
+    output:{ 
+        path:path.resolve(__dirname,'dist'),
+        filename:'js/bundle.js'
+    },
+
+    devServer:{
+
+        contentBase:'./dist'
+    },
+
+    plugins:[
+ 
+        new HtmlWebpackpPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        }) 
+    ],
+   module:{
+
+        rules:[
+            {
+
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:{
+
+                    loader: 'babel-loader'
+                }
+
+            }
+
+        ]
+    }
+    
+}
